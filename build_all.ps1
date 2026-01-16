8658e35002aeb11ace827d0cfa1a56440728ba05
kol <48543296+kol19pl@users.noreply.github.com>
2026-01-16 13:15:19 +0100

    dodano pakowanie pluginu

--- build_all.ps1
+++ build_all.ps1
@@ -9,7 +9,8 @@ $targetsWindows = @(
 #rustup target list
 $targetsLinux = @(
     "x86_64-unknown-linux-musl",
-    "x86_64-unknown-linux-gnu"
+    "x86_64-unknown-linux-gnu",
+    "x86_64-apple-darwin"
     #"aarch64-unknown-linux-musl"
     #"i686-unknown-linux-gnu",
     #"aarch64-unknown-linux-gnu",
